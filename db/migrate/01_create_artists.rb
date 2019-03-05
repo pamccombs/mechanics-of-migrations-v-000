cd72c4c24607e5a3c60bc1e85a221f91401f84b9
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-05 11:04:50 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -7,5 +7,15 @@ class CreateArtists < ActiveRecord::Migration
   :database => "db/artists.sqlite"
 )
 
-
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+ 
+ActiveRecord::Base.connection.execute(sql)
 end
