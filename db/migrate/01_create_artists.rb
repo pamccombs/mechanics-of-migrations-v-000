ea1caee7b1af511f1a4f9dc3b48ed7ec4213ea71
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-05 11:08:02 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,4 +2,13 @@ class CreateArtists < ActiveRecord::Migration
   def change
   create_table :artists do |t|
   end
+endclass CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
 end
