84dfe11a9afd0fe75656d6b7e8c5fd51d8e71467
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-05 11:06:44 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,16 +1,4 @@
 class CreateArtists < ActiveRecord::Migration
   def change
   end
-  
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
- 
-ActiveRecord::Base.connection.execute(sql)
 end
