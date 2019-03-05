721f82d1016070c9dcfedeb78763b52102d2c0df
Patrick McCombs <pa.mccombs1@gmail.com>
2019-03-05 11:04:39 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,11 @@
 class CreateArtists < ActiveRecord::Migration
   def change
   end
+  
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+
 end
